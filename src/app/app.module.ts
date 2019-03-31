2c7c751b1d7f515d78e2bc63fb10783415d02d78
Charles Geib <charles.r.geib@gmail.com>
2019-03-31 16:48:10 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Site1Component } from './sites/site1/site1.component';
+import { Site2Component } from './sites/site2/site2.component';
+import { Site3Component } from './sites/site3/site3.component';
+
+const appRoutes: Routes = [
+
+  { path: 'site1', component: Site1Component},
+  { path: 'site2/:name', component: Site2Component},
+  { path: 'site3/:firstName/:lastName', component: Site3Component},
+  { path: '', redirectTo: '/site1',  pathMatch: 'full'  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Site1Component,
+    Site2Component,
+    Site3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sites/site2/site2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site2',
+  templateUrl: './site2.component.html',
+  styleUrls: ['./site2.component.css']
+})
+export class Site2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
